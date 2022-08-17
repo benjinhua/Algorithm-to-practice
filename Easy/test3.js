61d3a3e8fadf85b03c311927873601f7f3df3e50
benjinhua <benjinhua66@gmail.com>
2022-08-17 21:34:18 +0800

    test3

--- Easy/test3.js
+++ Easy/test3.js
@@ -14,6 +14,15 @@
 提示：
 a, b 均可能是负数或 0
 结果不会溢出 32 位整数
+
+使用位运算，leetcode官方解答
+
+利用位运算来解决A+B的问题，可以将此问题转化为解决不进位相加和进位(carry bit)的两部分问题：
+1. 首先是不进位相加：_A = A^B 先对A和B进行异或运算(XOR manupitation) , A 和B 中两位不相同的变为1，相同的变为 0， 同为1是不进位
+2. 其次是进位  _B =(A&B)<<1
+所以 A+B = A^B + (A&B)<<1
+3.在循环中(A&B)<<1 的目的是将要进位的1一直向左移动，之后与新生成的_A进行异或运算，来进行进位运算的模拟（这一步就是进位）
+4. 直到将所有的1全部向左移位为0
  */
 
 /**
@@ -22,5 +31,15 @@ a, b 均可能是负数或 0
  * @return {number}
  */
  var add = function(a, b) {
-
+    while (b != 0) {
+        console.log('b1-',b);
+        const carry = (a & b) << 1;
+        console.log('b',b);
+        a = a ^ b;
+        console.log('a',a);
+        b = carry;
+    }
+    return a;
 };
+
+console.log(add(52,23));
